c73f2a56a2efc73f7742c29adc36a3c0c1ea381b
Michael-Gichamu <michaelmatere606@gmail.com>
2023-07-29 07:54:06 +0000

    Flask Application

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Flask Application"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -16,6 +16,12 @@ default_port = os.environ.get('HBNB_API_PORT', '5000')
 def db_teardown(exception):
     """Close Storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handles 404 error"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
