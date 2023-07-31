6c490164c00d730eb276cc8ee87e7369a03ad679
Michael-Gichamu <michaelmatere606@gmail.com>
2023-07-31 06:07:26 +0000

    Flask Application

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """Flask Application"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 default_host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
 default_port = os.environ.get('HBNB_API_PORT', '5000')
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
@@ -21,7 +23,7 @@ def db_teardown(exception):
 @app.errorhandler(404)
 def not_found(error):
     """Handles 404 error"""
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
