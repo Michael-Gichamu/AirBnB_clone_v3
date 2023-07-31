ad2ce6179ce4d712df050ede74eb912b8ab9ee8a
Michael-Gichamu <michaelmatere606@gmail.com>
2023-07-31 07:52:23 +0000

    City view

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -69,7 +69,7 @@ def create_city(state_id):
     return make_response(jsonify(city.to_dict()), 201)
 
 
-@app_views.route('/api/v1/cities/<city_id>', methods=['PUT'],
+@app_views.route('/cities/<city_id>', methods=['PUT'],
                  strict_slashes=False)
 def update_city(city_id):
     """
